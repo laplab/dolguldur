6d8b45fdd39ff46da60c8536930ef2d1ef18aee0
Nikita Lapkov <nikita.lapkov@gmail.com>
2022-06-24 12:45:29 -0400

    Example of JIT linking symbols from dynamic library

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 BASE='/usr/local/opt/llvm@12/bin'
 CLANG="$BASE/clang++"
 LLVM_CONFIG="$BASE/llvm-config"
-$CLANG -g lib.cpp main.cpp `$LLVM_CONFIG --cxxflags --ldflags --system-libs --libs core orcjit native` -O3 -o main
+$CLANG -g lib.cpp -O3 -shared -undefined dynamic_lookup -o liblib.so
+$CLANG -g ./liblib.so main.cpp `$LLVM_CONFIG --cxxflags --ldflags --system-libs --libs core orcjit native` -O3 -o main
